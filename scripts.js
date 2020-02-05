4351677cc80c7e35d9d4aa625ebdb3fc029980e4
jopakka <jopakkamies@gmail.com>
2020-02-05 19:58:01 +0200

    create user works

--- scripts.js
+++ scripts.js
@@ -1,6 +1,21 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyCshPRphIpHeg5sj1i6O0K8iHcWxr5JVFQ",
+    authDomain: "cardgame-project.firebaseapp.com",
+    databaseURL: "https://cardgame-project.firebaseio.com",
+    projectId: "cardgame-project",
+    storageBucket: "cardgame-project.appspot.com",
+    messagingSenderId: "576386674320",
+    appId: "1:576386674320:web:0f8db7c2c8550a0cfbb215",
+    measurementId: "G-45VCDFQGH3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore();
+
+
 let deck_id;
 
-function getDeck(amount){
+function getDeck(amount) {
     let url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=" + amount;
 
     let playerCards = document.getElementById("playerCards");
@@ -15,7 +30,7 @@ function getDeck(amount){
         .catch((error) => console.log(error));
 }
 
-function drawCard(amount){
+function drawCard(amount) {
     let url = "https://deckofcardsapi.com/api/deck/" + deck_id + "/draw/?count=" + amount;
 
     fetch(url)
@@ -24,12 +39,12 @@ function drawCard(amount){
         .catch((error) => console.log(error));
 }
 
-function addToHand(json){
+function addToHand(json) {
     console.log(json);
     let url = "https://deckofcardsapi.com/api/deck/" + deck_id + "/pile/" + "player1" + "/add/?cards=";
 
-    for(let i = 0; i < json.cards.length; i++){
-        if(i === json.cards.length - 1){
+    for (let i = 0; i < json.cards.length; i++) {
+        if (i === json.cards.length - 1) {
             url += json.cards[i].code;
         } else {
             url += json.cards[i].code + ",";
@@ -42,7 +57,7 @@ function addToHand(json){
         .catch((error) => console.log(error));
 }
 
-function listhand(){
+function listhand() {
     let url = "https://deckofcardsapi.com/api/deck/" + deck_id + "/pile/" + "player1" + "/list/";
 
     fetch(url)
@@ -50,7 +65,7 @@ function listhand(){
         .then((json) => {
             let playerCards = document.getElementById("playerCards");
             playerCards.innerHTML = "";
-            for(let i = 0; i < json.piles.player1.cards.length; i++){
+            for (let i = 0; i < json.piles.player1.cards.length; i++) {
                 let img = document.createElement("img");
                 img.className = "card";
                 img.src = "cards/" + json.piles.player1.cards[i].code + ".png";
@@ -59,4 +74,31 @@ function listhand(){
 
         })
         .catch((error) => console.log(error));
+}
+
+function createUser() {
+    let email = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    email += "@randomemail.com";
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        alert(errorMessage);
+    });
+}
+
+function login() {
+    let email = document.getElementById("username2").value;
+    let password = document.getElementById("password2").value;
+
+    email += "@randomemail.com";
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+    });
 }
